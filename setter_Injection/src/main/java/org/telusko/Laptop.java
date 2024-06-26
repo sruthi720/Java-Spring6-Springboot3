d01b95bf9f36b4f03e3f409e6ee8872b67d87486
Sruthi.I.R <sruthiir720@gmail.com>
2024-06-26 14:51:57 +0530

    done

---
+++ setter_Injection/src/main/java/org/telusko/Laptop.java
@@ -0,0 +1,10 @@
+package org.telusko;
+
+public class Laptop {
+    public Laptop() {
+        System.out.println("Laptop object created");
+    }
+    public void compile(){
+        System.out.println("compile !");
+    }
+}
