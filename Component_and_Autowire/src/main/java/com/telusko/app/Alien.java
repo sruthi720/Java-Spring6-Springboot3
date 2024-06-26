cf5e171d9643759d236e0be8b2fdeb2d054348aa
Sruthi.I.R <sruthiir720@gmail.com>
2024-06-26 14:49:50 +0530

    done

---
+++ Component_and_Autowire/src/main/java/com/telusko/app/Alien.java
@@ -0,0 +1,14 @@
+package com.telusko.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien {
+    @Autowired
+    Laptop laptop;
+    public void code(){
+        laptop.compile();
+        System.out.println("coding");
+    }
+}
---
+++ Component_and_Autowire/src/main/java/com/telusko/app/SpringBootDemoApplication.java
@@ -0,0 +1,17 @@
+package com.telusko.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context =SpringApplication.run(SpringBootDemoApplication.class, args);
+		Alien obj =context.getBean(Alien.class);
+		obj.code();
+	}
+
+}
