38ed7d62cc93eb0917e3c335ecfe3b572eeb9e34
Sruthi.I.R <sruthiir720@gmail.com>
2024-07-01 14:49:12 +0530

    Java based @configuration, @Bean-- name attribute, @scope

--- java based config/src/main/java/org/telusko/App.java
+++ java based config/src/main/java/org/telusko/App.java
@@ -1,7 +1,7 @@
 package org.telusko;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * Hello world!
@@ -11,16 +11,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Desktop dt = context.getBean("COM1", Desktop.class);
+        dt.compile();
 
-        Alien obj = (Alien)  context.getBean("alien"); //if you want object from the container use this code
-//        OR
-//        Alien obj = context.getBean("alien", Alien.class);
-//        OR
-//        Alien obj = context.getBean(Alien.class);
-        obj.code();
-//        Computer com = context.getBean(Computer.class);// which one is primary, that will work else error,because of confusion
-        Computer com = context.getBean(Desktop.class);
+        Desktop dt1 = context.getBean("system", Desktop.class);
+        dt1.compile();
+
+
 
     }
 }
