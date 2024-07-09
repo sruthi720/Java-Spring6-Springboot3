89326ef2d4bcc4d981de1a7cd1414e4c167cb23e
Sruthi.I.R <sruthiir720@gmail.com>
2024-07-09 21:30:32 +0530

    Springboot project

--- Model_attribute/src/main/java/com/telusko/SpringBootWeb1/HomeController.java
+++ Model_attribute/src/main/java/com/telusko/SpringBootWeb1/HomeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
-    @ModelAttribute("language")
+    @ModelAttribute("language   ")
     public String courseName(){
         return "Java";
     }
