e48e764d55324c8d49d65e70e77c79085cf40efa
Sruthi.I.R <sruthiir720@gmail.com>
2024-07-13 21:45:23 +0530

    done

---
+++ jobportal/springboot-rest/src/test/java/com/teslusko/springboot_rest/SpringbootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teslusko.springboot_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
